1789ce9e47da07526af01580e36df277db69829d
RandenBanuelos <56743748+RandenBanuelos@users.noreply.github.com>
2020-03-07 22:42:49 -0800

    Visual Novel Cutscenes and End Scene

---
+++ ContinueButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueButton : MonoBehaviour
+{
+    float lengthOfTime = 3 * 60.0f;
+
+
+    GameObject button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GameObject.Find("FinalSceneButton");
+        ButtonHidden();
+        Invoke("ButtonRevealed", lengthOfTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ButtonHidden()
+    {
+        button.SetActive(false);
+    }
+
+    void ButtonRevealed()
+    {
+        button.SetActive(true);
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("EndCutscene");
+    }
+}
